fa22ad0f53b77f87a8047db2cff93f1463b0fc81
Memory <memory78078@gmail.com>
2017-01-12 16:44:35 +0800

    turn card - two card

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
-import * as Hammer from 'hammerjs';
-
+export enum Mode {
+    TWOCARD, FLIP
+}
 
 @Component({
     moduleId: module.id,
@@ -10,47 +11,33 @@ import * as Hammer from 'hammerjs';
     styleUrls: ['app.component.css']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-    orderArr: Array<number> = [-1, 0, 1];
-    tableList: Array<HTMLDivElement> = [];
-    tableWidth: number;
-    @ViewChild("sliderBox") sliderBox: ElementRef;
 
-    ngOnInit(){}
+    readonly InitCardStyle: any = {
+        transform: `translate(0, 0)`
+    };
+    cardStyle: any = Object.assign({}, this.InitCardStyle);
 
-    ngAfterViewInit() {
-        let tables = (<HTMLDivElement>this.sliderBox.nativeElement).children;
-        for(let i = 0; i < tables.length; i++){
-            this.tableList.push(<HTMLDivElement>tables[i]);
-        }
-        this.tableWidth = this.tableList[0].offsetWidth;
-        this.setTablesStyle();
-    }
+    mode: Mode = Mode.TWOCARD;
 
-    setTablesStyle(deltaX: number = 0){
-        this.tableList.forEach((table, index)=>{
-            let posX: number = this.orderArr[index] * this.tableWidth + deltaX;
-            table.setAttribute('style', `transform: translate3d(${posX}px, 0, 0)`);
-        });
-    }
+    modeType = Mode;
 
-    handlePan(e: HammerInput){
-        if(e.type === 'pan'){
-            this.setTablesStyle(e.deltaX);
-        }else if(e.type === 'panend'){
-            if(e.distance > this.tableWidth / 2 || e.velocity > 0.3){
-                this.switchTable(e.deltaX > 0? 'right' : 'left');
-            }
-            this.setTablesStyle();
+    ngOnInit() { }
+
+    ngAfterViewInit() { }
+
+    handlePan(e: HammerInput) {
+        console.log("pan", e);
+        if (e.type === "panstart") {
+            this.cardStyle = Object.assign({}, this.InitCardStyle);
+        } else if (e.isFinal) {
+            this.cardStyle.transition = "transform 100ms ease-out";
+            this.cardStyle.transform = `translate(0, 0)`;
+        } else {
+            this.cardStyle.transform = `translate(${e.deltaX}px, ${e.deltaY}px)`;
         }
     }
 
-    switchTable(direction: 'right' | 'left'){
-        if(direction === 'right'){
-            let tmp: HTMLDivElement = this.tableList.pop();
-	        this.tableList.unshift(tmp);
-        } else if(direction === 'left') {
-            let tmp: HTMLDivElement = this.tableList.shift();
-	        this.tableList.push(tmp);
-        }
+    changeMode() {
+        this.mode = this.mode === Mode.TWOCARD ? Mode.FLIP : Mode.TWOCARD;
     }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,22 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from "@angular/platform-browser";
+import { AppComponent } from './app.component';
 
-import { AppComponent }  from './app.component';
+export class AppHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    "pan": { threshold: 1 }
+  }
+}
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: AppHammerConfig
+    },
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
